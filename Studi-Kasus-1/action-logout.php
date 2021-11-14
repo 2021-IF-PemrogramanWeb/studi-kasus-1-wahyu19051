dea6af50b0ba2d958e9c90232ed432dd6dfc57e0
wahyu19051 <90163337+wahyu19051@users.noreply.github.com>
2021-11-14 14:23:48 +0700

    Update action-logout.php

--- Studi-Kasus-1/action-logout.php
+++ Studi-Kasus-1/action-logout.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    $_SESSION['username'] = '';
-    unset($_SESSION['username']);
+    $_SESSION['login'] = '';
+    unset($_SESSION['login']);
     session_unset();
     session_destroy();
     header("Location: index.html");
